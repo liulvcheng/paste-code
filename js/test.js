583bb94eac9045d19d8ac77edf17f356738c2040
吉凶歹徒 <luliu8322@gmail.com>
2024-11-01 11:24:55 +0800

    更新 test.js

---
+++ js/test.js
@@ -0,0 +1,8 @@
+[
+    {id: 1,name:"liu1",age:11,sex:1,info:1},
+    {id: 1,name:"liu1",age:11,sex:2,info:2},
+    {id: 1,name:"liu1",age:11,sex:1,info:1},
+    {id: 1,name:"liu12",age:22,sex:2,info:2},
+    {id: 1,name:"liu12",age:22,sex:1,info:1},
+    {id: 1,name:"liu12",age:22,sex:2,info:2},
+]
